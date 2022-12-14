011a81bb5490e6e7a3620381174004bc2f2c4204
juni998 <wodnrdlgla@gmail.com>
2022-12-14 16:52:35 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/com/trip100/trip100Application.java
@@ -0,0 +1,12 @@
+package com.trip100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class trip100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(trip100Application.class, args);
+    }
+}
