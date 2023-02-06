02dbd55d46c8e37ce888bd59408f6616d86d910b
juni998 <wodnrdlgla@gmail.com>
2023-02-06 15:55:08 +0900

    feat: 배송 서비스 추가

---
+++ src/main/java/trip100/domain/delivery/Delivery.java
@@ -0,0 +1,38 @@
+package trip100.domain.delivery;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import trip100.domain.address.Address;
+import trip100.domain.order.Order;
+
+import javax.persistence.*;
+
+@Getter
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+
+    @Builder
+    public Delivery(Order order, Address address, DeliveryStatus status) {
+        this.order = order;
+        this.address = address;
+        this.status = status;
+    }
+}
---
+++ src/main/java/trip100/domain/delivery/DeliveryStatus.java
@@ -0,0 +1,6 @@
+package trip100.domain.delivery;
+
+public enum DeliveryStatus {
+
+    READY, COMP
+}
